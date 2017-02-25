b6d90b8eb7b54cb0d40c810f6b218917344a3f93
Mark Hanna <mark@skvare.com>
2017-02-25 17:33:20 -0600

    sync from 2.x branch latest

--- templates/civicrm-event.tpl.php
+++ templates/civicrm-event.tpl.php
@@ -31,14 +31,11 @@
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php if (!$page): ?>
     <h2<?php print $title_attributes; ?>>
-        <a href="<?php print $url; ?>"><?php print $title; ?></a>
+      <a href="<?php print $url; ?>"><?php print $title; ?></a>
     </h2>
   <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
-    <?php
-
-      print render($content);
-    ?>
+    <?php print render($content); ?>
   </div>
 </div>
