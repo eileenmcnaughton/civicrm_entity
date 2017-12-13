d02aa9f39d8f476c97d5763af4a72bc383678ffb
Matt Glaman <nmd.matt@gmail.com>
2017-12-13 08:45:00 -0600

    Add info, services stub.

---
+++ src/CiviEntityStorage.php
@@ -0,0 +1,6 @@
+<?php
+// @todo This will have the entity storage and tap into the CiviCRM api kernel.
+// @todo inject civicrm_entity.civicrm_container
+
+// In construct make sure to invoke initialize
+//
