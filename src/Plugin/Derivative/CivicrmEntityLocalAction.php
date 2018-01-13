7b1374fad458517a09c26f0322e93b244502c5c0
Matt Glaman <nmd.matt@gmail.com>
2018-01-13 13:50:23 -0600

    Generate local actions and tasks for entities

---
+++ src/Plugin/Derivative/CivicrmEntityLocalAction.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\civicrm_entity\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\Routing\RouteProviderInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides local action definitions for all CiviCRM entity definitions.
+ */
+class CivicrmEntityLocalAction extends DeriverBase implements ContainerDeriverInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The entity manager
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a CivicrmEntityLocalAction object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
+   *   The entity manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_manager) {
+    $this->entityTypeManager = $entity_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $this->derivatives = [];
+
+    $civicrm_entities = array_filter($this->entityTypeManager->getDefinitions(), function (EntityTypeInterface $type) {
+      return $type->getProvider() == 'civicrm_entity' && $type->get('civicrm_entity');
+    });
+
+    /** @var \Drupal\Core\Entity\EntityTypeInterface $entity_type */
+    foreach ($civicrm_entities as $entity_type_id => $entity_type) {
+      $this->derivatives["field_storage_config_add_$entity_type_id"] = [
+        'route_name' => "entity.$entity_type_id.add_form",
+        'title' => $this->t('Add :label', [':label' => $entity_type->getLabel()]),
+        'appears_on' => ["entity.$entity_type_id.collection"],
+      ] + $base_plugin_definition;
+    }
+
+    return $this->derivatives;
+  }
+
+}
---
+++ src/Plugin/Derivative/DynamicLocalTasks.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Drupal\civicrm_entity\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\content_moderation\ModerationInformationInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Generates moderation-related local tasks.
+ */
+class DynamicLocalTasks extends DeriverBase implements ContainerDeriverInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The base plugin ID.
+   *
+   * @var string
+   */
+  protected $basePluginId;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Creates an FieldUiLocalTask object.
+   *
+   * @param string $base_plugin_id
+   *   The base plugin ID.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
+   *   The translation manager.
+   */
+  public function __construct($base_plugin_id, EntityTypeManagerInterface $entity_type_manager, TranslationInterface $string_translation) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->stringTranslation = $string_translation;
+    $this->basePluginId = $base_plugin_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $base_plugin_id,
+      $container->get('entity_type.manager'),
+      $container->get('string_translation')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $this->derivatives = [];
+
+    $civicrm_entities = array_filter($this->entityTypeManager->getDefinitions(), function (EntityTypeInterface $type) {
+      return $type->getProvider() == 'civicrm_entity' && $type->hasKey('civicrm_entity');
+    });
+
+    foreach ($civicrm_entities as $entity_type_id => $entity_type) {
+      $this->derivatives["$entity_type_id.canonical"] = [
+          'route_name' => "entity.$entity_type_id.canonical",
+          'title' => $this->t('View'),
+          'base_route' => "entity.$entity_type_id.canonical",
+        ] + $base_plugin_definition;
+      $this->derivatives["$entity_type_id.edit_form"] = [
+          'route_name' => "entity.$entity_type_id.edit_form",
+          'title' => $this->t('Edit'),
+          'base_route' => "entity.$entity_type_id.canonical",
+        ] + $base_plugin_definition;
+      $this->derivatives["$entity_type_id.delete_form"] = [
+          'route_name' => "entity.$entity_type_id.delete_form",
+          'title' => $this->t('Delete'),
+          'base_route' => "entity.$entity_type_id.canonical",
+          'weight' => 10,
+        ] + $base_plugin_definition;
+    }
+
+    return $this->derivatives;
+  }
+
+}
