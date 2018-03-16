c9bdecb8590b4bf1b9e0e204141b0c0068504c5b
Matt Glaman <nmd.matt@gmail.com>
2018-03-15 21:24:33 -0500

    Add CiviCRM form

--- src/Form/CivicrmEntityForm.php
+++ src/Form/CivicrmEntityForm.php
@@ -2,16 +2,80 @@
 
 namespace Drupal\civicrm_entity\Form;
 
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class CivicrmEntityForm extends ContentEntityForm {
+
+  /**
+   * The Current User object.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a NodeForm object.
+   *
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   *   The entity manager.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle service.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   */
+  public function __construct(EntityManagerInterface $entity_manager, EntityTypeBundleInfoInterface $entity_type_bundle_info, TimeInterface $time, AccountInterface $current_user) {
+    parent::__construct($entity_manager, $entity_type_bundle_info, $time);
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.manager'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('datetime.time'),
+      $container->get('current_user')
+    );
+  }
+
   /**
    * {@inheritdoc}
    */
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
     $form['#tree'] = TRUE;
+    $form['#theme'] = ['civicrm_entity_entity_form'];
+    $form['#attached']['library'][] = 'civicrm_entity/form';
+
+    $form['advanced'] = [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['entity-meta']],
+      '#weight' => 99,
+    ];
+    $form['meta'] = [
+      '#attributes' => ['class' => ['entity-meta__header']],
+      '#type' => 'container',
+      '#group' => 'advanced',
+      '#weight' => -100,
+    ];
+    $form['options'] = [
+      '#type' => 'details',
+      '#title' => t('Options'),
+      '#group' => 'advanced',
+      '#weight' => 95,
+      '#optional' => TRUE,
+    ];
+
     return $form;
   }
 
