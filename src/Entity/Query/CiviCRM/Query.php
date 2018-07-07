b5ce9e2dcbbb266ddf0fadf3e413aea61d5d643d
Matt Glaman <nmd.matt@gmail.com>
2018-07-07 00:17:18 -0500

    Support field config cardinality

--- src/Entity/Query/CiviCRM/Query.php
+++ src/Entity/Query/CiviCRM/Query.php
@@ -28,6 +28,14 @@ class Query extends QueryBase implements QueryInterface {
   public function execute() {
     $params = [];
     foreach ($this->condition->conditions() as $condition) {
+      // If there's anything requiring a custom field, set condition which cannot
+      // be completed.
+      // @todo Introduced when supporting field config. Find something better.
+      // @see \Drupal\field_ui\Form\FieldStorageConfigEditForm::validateCardinality()
+      if (substr($condition['field'], 0, 6) === 'field_') {
+        $params['id'] = '-1';
+        break;
+      }
       $operator = $condition['operator'] ?: '=';
       if ($operator != '=') {
         $params[$condition['field']] = [$operator => $condition['value']];
