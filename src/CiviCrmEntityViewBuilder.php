a0dd477c53e24eb40c46888007b76ac0432261b2
Arshad Chummun <arshad@chummun.com>
2018-08-09 03:28:18 +0400

    Make civicrm entities themable and add a default civicrm-entity template

---
+++ src/CiviCrmEntityViewBuilder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\civicrm_entity;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityViewBuilder;
+
+class CiviCrmEntityViewBuilder extends EntityViewBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getBuildDefaults(EntityInterface $entity, $view_mode) {
+    $defaults = parent::getBuildDefaults($entity, $view_mode);
+
+    // Set a default theme.
+    $defaults['#theme'] = 'civicrm_entity';
+
+    return $defaults;
+  }
+
+}
