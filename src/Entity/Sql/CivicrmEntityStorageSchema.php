271f0d9c7975c5af804ef4412e09d31e67bf0302
bklaver <bklaver@echidna.ca>
2019-05-23 10:18:55 -0400

    Update CivicrmEntityStorageSchema.php

--- src/Entity/Sql/CivicrmEntityStorageSchema.php
+++ src/Entity/Sql/CivicrmEntityStorageSchema.php
@@ -5,9 +5,12 @@ namespace Drupal\civicrm_entity\Entity\Sql;
 use Drupal\civicrm_entity\CiviEntityStorage;
 use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\ContentEntityTypeInterface;
-use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\Sql\SqlContentEntityStorageSchema;
+use Drupal\Core\Entity\Sql\TableMappingInterface;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
 
 /**
  * Implementation of the SqlContentEntityStorageSchema for CiviCRM entities.
@@ -34,21 +37,29 @@ class CivicrmEntityStorageSchema extends SqlContentEntityStorageSchema {
   /**
    * Constructs a CivicrmEntityStorageSchema.
    *
-   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
-   *   The entity manager.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
    * @param \Drupal\Core\Entity\ContentEntityTypeInterface $entity_type
    *   The entity type.
    * @param \Drupal\civicrm_entity\CiviEntityStorage $storage
    *   The storage of the entity type. This must be an SQL-based storage.
    * @param \Drupal\Core\Database\Connection $database
    *   The database connection to be used.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   The entity field manager.
    */
-  public function __construct(EntityManagerInterface $entity_manager, ContentEntityTypeInterface $entity_type, CiviEntityStorage $storage, Connection $database) {
-    $this->entityManager = $entity_manager;
-    $this->entityType = $entity_type;
-    $this->fieldStorageDefinitions = $entity_manager->getFieldStorageDefinitions($entity_type->id());
-    $this->storage = $storage;
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ContentEntityTypeInterface $entity_type, CiviEntityStorage $storage, Connection $database, EntityFieldManagerInterface $entity_field_manager = NULL) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->storage = clone $storage;
     $this->database = $database;
+    if (!$entity_field_manager) {
+      @trigger_error('Calling SqlContentEntityStorageSchema::__construct() with the $entity_field_manager argument is supported in drupal:8.7.0 and will be required before drupal:9.0.0. See https://www.drupal.org/node/2549139.', E_USER_DEPRECATED);
+      $entity_field_manager = \Drupal::service('entity_field.manager');
+    }
+    $this->entityFieldManager = $entity_field_manager;
+
+    $this->entityType = $entity_type_manager->getActiveDefinition($entity_type->id());
+    $this->fieldStorageDefinitions = $entity_field_manager->getActiveFieldStorageDefinitions($entity_type->id());
   }
 
   /**
@@ -81,12 +92,26 @@ class CivicrmEntityStorageSchema extends SqlContentEntityStorageSchema {
     return FALSE;
   }
 
+
   /**
    * {@inheritdoc}
+   This will get rid of the "Mismatched entity and/or field definitions
+The following changes were detected in the entity type and field definitions." 
+Message on the status page. I don't think it should be left in though.
+
+  public function requiresFieldStorageSchemaChanges(FieldStorageDefinitionInterface $storage_definition, FieldStorageDefinitionInterface $original) {
+    return false;
+  }
+  */
+
+  /**
+   * @param \Drupal\Core\Entity\Sql\TableMappingInterface $table_mapping
+   *   A table mapping object.
    *
-   * The entity table schema is managed by CiviCRM.
+   * @return array
+   *   The entity table schema is managed by CiviCRM.
    */
-  protected function getEntitySchemaTables() {
+  protected function getEntitySchemaTables(TableMappingInterface $table_mapping) {
     return [];
   }
 
