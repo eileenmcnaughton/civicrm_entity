699b007363574d7f38990ccf5bf1f1a5e20e913f
Rob Ellis <rob1263@gmail.com>
2020-10-27 01:15:03 +0000

    Changed views arg default plugin for contact subtype to use dependency injection for services

--- src/Plugin/views/argument_default/ContactSubtype.php
+++ src/Plugin/views/argument_default/ContactSubtype.php
@@ -5,6 +5,9 @@ namespace Drupal\civicrm_entity\Plugin\views\argument_default;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountProxy;
+use Drupal\civicrm_entity\CiviCrmApiInterface;
 
 /**
  * Default argument plugin to extract the current user's civicrm contact subtype
@@ -19,23 +22,64 @@ use Drupal\views\Plugin\views\argument_default\ArgumentDefaultPluginBase;
 class ContactSubtype extends ArgumentDefaultPluginBase implements CacheableDependencyInterface {
 
   /**
+   * Drupal\Core\Session\AccountProxy definition.
+   *
+   * @var \Drupal\Core\Session\AccountProxy
+   */
+  protected $currentUser;
+
+  /**
+   * The CiviCRM API.
+   *
+   * @var \Drupal\civicrm_entity\CiviCrmApiInterface
+   */
+  protected $civicrmApi;
+
+  /**
+   * Constructs a new ContactSubtype instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param Drupal\Core\Session\AccountProxy
+   *   The current user.
+   * @param Drupal\civicrm_entity\CiviCrmApiInterface $civicrmApi
+   *   The CiviCRM Api.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountProxy $currentUser, CiviCrmApiInterface $civicrmApi) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $currentUser;
+    $this->civicrmApi = $civicrmApi;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition,
+      $container->get('current_user'),
+      $container->get('civicrm_entity.api'));
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getArgument() {
 
     $current_user_contact_subtype = '<none>';
 
-    $civicrm_api = \Drupal::getContainer()->get('civicrm_entity.api');                                                 
-
-    $results = $civicrm_api->get('UFMatch', [
+    $results = $this->civicrmApi->get('UFMatch', [
       'sequential' => 1,
-      'id' => \Drupal::currentUser()->id(),
+      'id' => $this->currentUser->id(),
     ]);
 
     if (!empty($results) && !empty($results[0]['contact_id'])) {
       $cid = $results[0]['contact_id'];
 
-      $results = $civicrm_api->get('contact', [
+      $results = $this->civicrmApi->get('contact', [
         'sequential' => 1,
         'return' => ['contact_sub_type'],
         'id' => $cid,
