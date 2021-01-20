6165c6a2e31f4e583ed8c24fae664071fb926f1e
puresyntax71 <34715246+puresyntax71@users.noreply.github.com>
2021-01-21 00:09:35 +0800

    Add fixes for pledge API.

--- src/CiviEntityStorage.php
+++ src/CiviEntityStorage.php
@@ -282,6 +282,11 @@ class CiviEntityStorage extends SqlContentEntityStorage {
    * {@inheritdoc}
    */
   public function hasData() {
+    if (($component = $this->entityType->get('component')) !== NULL) {
+      $components = $this->getCiviCrmApi()->getValue('Setting', ['name' => 'enable_components']);
+      return !in_array($component, $components) ? FALSE :
+        $this->getCiviCrmApi()->getCount($this->entityType->get('civicrm_entity')) > 0;
+    }
     return $this->getCiviCrmApi()->getCount($this->entityType->get('civicrm_entity')) > 0;
   }
   /**
