6c916dfc75da06a46556e65304bbeccead206cca
Matt Glaman <nmd.matt@gmail.com>
2021-01-24 23:21:51 -0600

    Fix upgrade

--- civicrm_entity.post_update.php
+++ civicrm_entity.post_update.php
@@ -6,7 +6,7 @@
  */
 
 /**
- * Implements hook_post_update_NAME().
+ * Enable bundle support for CiviCRM Entity entity types.
  */
 function civicrm_entity_post_update_entity_bundles(&$sandbox) {
   // An empty post_update hook triggers a cache rebuild, which ensures that
