71e8ce07b2992169475aa33b1dd3c25078f0c4ec
Matt Glaman <nmd.matt@gmail.com>
2021-04-11 08:43:44 -0500

    Remove add_page, simplify adding bundle supported entities

--- src/Routing/CiviCrmEntityRouteProvider.php
+++ src/Routing/CiviCrmEntityRouteProvider.php
@@ -18,9 +18,24 @@ class CiviCrmEntityRouteProvider extends AdminHtmlRouteProvider {
     if ($entity_type->get('civicrm_entity_ui_exposed')) {
       return parent::getRoutes($entity_type);
     }
-    else {
-      return new RouteCollection();
+
+    return new RouteCollection();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getAddFormRoute(EntityTypeInterface $entity_type) {
+    $has_bundles = $entity_type->hasKey('bundle');
+    $entity_add_form_route = parent::getAddFormRoute($entity_type);
+    if ($has_bundles) {
+      // This ensures the form receives a default bundle from the
+      // CivicrmEntity::preCreate method, avoiding the need for the `add_page`
+      // route for selecting a bundle.
+      assert($entity_add_form_route !== NULL);
+      $entity_add_form_route->setDefault('bundle', $entity_type->id());
     }
+    return $entity_add_form_route;
   }
 
 }
--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -3,6 +3,7 @@
 namespace Drupal\civicrm_entity\Routing;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Drupal\Core\Routing\RoutingEvents;
 use Symfony\Component\Routing\RouteCollection;
@@ -20,19 +21,33 @@ final class RouteSubscriber extends RouteSubscriberBase {
   private $entityTypeManager;
 
   /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
    * RouteSubscriber constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *    The entity type manager.
+   *   The entity type manager.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ModuleHandlerInterface $module_handler) {
     $this->entityTypeManager = $entity_type_manager;
+    $this->moduleHandler = $module_handler;
   }
 
   /**
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
+    // Only run if Field UI is installed.
+    if (!$this->moduleHandler->moduleExists('field_ui')) {
+      return;
+    }
     foreach ($this->entityTypeManager->getDefinitions() as $entity_type_id => $entity_type) {
       if (!$entity_type->get('civicrm_entity_ui_exposed')) {
         continue;
