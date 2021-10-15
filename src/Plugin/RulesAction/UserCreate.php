9ab49edca4922071bc7cc3b0a05e5d90b1ca3679
olivierh65 <olivierh65@users.noreply.github.com>
2021-10-15 13:14:10 +0200

    Remove commas in context definition

--- src/Plugin/RulesAction/UserCreate.php
+++ src/Plugin/RulesAction/UserCreate.php
@@ -21,32 +21,32 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *      "contact_id" = @ContextDefinition("integer",
  *        label = @Translation("CiviCRM contact ID"),
  *        description = @Translation("The CiviCRM contact ID."),
- *        required = TRUE,
+ *        required = TRUE
  *      ),
  *      "is_active" = @ContextDefinition("boolean",
  *        label = @Translation("Activate account"),
  *        description = @Translation("Set to TRUE to activate account. Leave empty to not activate the account. Defaults to TRUE."),
  *        assignment_restriction = "input",
  *        default_value = TRUE,
- *        required = FALSE,
+ *        required = FALSE
  *      ),
  *      "notify" = @ContextDefinition("boolean",
  *        label = @Translation("Send account notification email"),
  *        description = @Translation("Set to TRUE to send a notification email. Leave empty to not send an account notification email."),
  *        assignment_restriction = "input",
  *        default_value = TRUE,
- *        required = FALSE,
+ *        required = FALSE
  *      ),
  *      "signin" = @ContextDefinition("boolean",
  *        label = @Translation("Instant signin"),
  *        description = @Translation("Set to TRUE to automatically log in the user. Leave empty to not automatically log in the user."),
  *        assignment_restriction = "input",
  *        default_value = TRUE,
- *        required = FALSE,
+ *        required = FALSE
  *      ),
  *      "format" = @ContextDefinition("string",
  *        label = @Translation("Format"),
- *        description = @Translation("Format of the username."),
+ *        description = @Translation("Format of the username.")
  *      )
  *   },
  *   provides = {
