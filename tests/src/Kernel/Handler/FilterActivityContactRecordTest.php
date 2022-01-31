a208606493515508384088dabea98b085a66887a
puresyntax71 <34715246+puresyntax71@users.noreply.github.com>
2022-02-01 03:11:01 +0800

    Update scenarios.

--- tests/src/Kernel/Handler/FilterActivityContactRecordTest.php
+++ tests/src/Kernel/Handler/FilterActivityContactRecordTest.php
@@ -11,23 +11,109 @@ use Drupal\views\Views;
  */
 final class FilterActivityContactRecordTest extends KernelHandlerTestBase {
 
-  public static $testViews = ['test_view'];
+  public static $testViews = ['test_activity'];
 
   protected $columnMap = [
-    'contact_type' => 'contact_type',
-    'display_name' => 'display_name',
+    'subject' => 'subject',
+    'location' => 'location',
+    'details__value' => 'details__value',
   ];
 
   /**
-   * Tests general offset.
+   * Tests simple operator.
    */
-  public function testOffset() {
-  }
+  public function testFilter() {
+    $view = Views::getView('test_activity');
 
-  /**
-   * Tests the filter operator between/not between.
-   */
-  protected function testBetween() {
+    $view->setDisplay();
+    $view->displayHandlers->get('default')->overrideOption('filters', [
+      'assignee_id' => [
+        'id' => 'assignee_id',
+        'table' => 'civicrm_activity',
+        'field' => 'assignee_id',
+        'value' => ['value' => 4],
+        'operator' => '=',
+        'entity_type' => 'civicrm_activity',
+        'entity_field' => 'assignee_id',
+        'plugin_id' => 'civicrm_entity_civicrm_activity_contact_record',
+      ],
+    ]);
+
+    $this->executeView($view);
+
+    $expected_result = [
+      [
+        'subject' => 'Email about new seeds',
+        'location' => 'Texas',
+        'details__value' => 'We just got new seeds.',
+      ],
+    ];
+
+    $this->assertCount(1, $view->result);
+    $this->assertIdenticalResultset($view, $expected_result, $this->columnMap);
+
+    $view->destroy();
+
+    $view->setDisplay();
+    $view->displayHandlers->get('default')->overrideOption('filters', [
+      'source_contact_id' => [
+        'id' => 'source_contact_id',
+        'table' => 'civicrm_activity',
+        'field' => 'source_contact_id',
+        'value' => ['value' => 3],
+        'operator' => '=',
+        'entity_type' => 'civicrm_activity',
+        'entity_field' => 'source_contact_id',
+        'plugin_id' => 'civicrm_entity_civicrm_activity_contact_record',
+      ],
+    ]);
+
+    $this->executeView($view);
+
+    $expected_result = [
+      [
+        'subject' => 'Meeting about new seeds',
+        'location' => 'Pennsylvania',
+        'details__value' => 'We need to find more seeds!',
+      ],
+      [
+        'subject' => 'Email about new seeds',
+        'location' => 'Texas',
+        'details__value' => 'We just got new seeds.',
+      ],
+    ];
+
+    $this->assertCount(2, $view->result);
+    $this->assertIdenticalResultset($view, $expected_result, $this->columnMap);
+
+    $view->destroy();
+
+    $view->setDisplay();
+    $view->displayHandlers->get('default')->overrideOption('filters', [
+      'target_id' => [
+        'id' => 'target_id',
+        'table' => 'civicrm_activity',
+        'field' => 'target_id',
+        'value' => ['value' => 4],
+        'operator' => '=',
+        'entity_type' => 'civicrm_activity',
+        'entity_field' => 'target_id',
+        'plugin_id' => 'civicrm_entity_civicrm_activity_contact_record',
+      ],
+    ]);
+
+    $this->executeView($view);
+
+    $expected_result = [
+      [
+        'subject' => 'Meeting about new seeds',
+        'location' => 'Pennsylvania',
+        'details__value' => 'We need to find more seeds!',
+      ],
+    ];
+
+    $this->assertCount(1, $view->result);
+    $this->assertIdenticalResultset($view, $expected_result, $this->columnMap);
   }
 
   /**
@@ -37,24 +123,39 @@ final class FilterActivityContactRecordTest extends KernelHandlerTestBase {
     /** @var \Drupal\civicrm_entity\CiviCrmApi $civicrm_api */
     $civicrm_api = $this->container->get('civicrm_entity.api');
 
-    $result = $civicrm_api->save('CustomGroup', [
-      'title' => 'Test',
-      'extends' => 'Individual',
-    ]);
-
-    $result = reset($result['values']);
-
-    $civicrm_api->save('CustomField', [
-      'custom_group_id' => $result['id'],
-      'label' => 'Test date',
-      "data_type" => 'Date',
-      "html_type" => 'Select Date',
-    ]);
-
-    $contacts = $this->createSampleData();
-
-    foreach ($contacts as $contact) {
+    foreach ($this->createSampleData() as $contact) {
       $civicrm_api->save('Contact', $contact);
+    }
+
+    $activities = [
+      [
+        'subject' => 'Meeting about new seeds',
+        'activity_type_id' => 'Meeting',
+        'location' => 'Pennsylvania',
+        'details' => 'We need to find more seeds!',
+        // Jane Smith.
+        'source_contact_id' => 3,
+        // Jane Smith.
+        'assignee_id' => 3,
+        // John Doe.
+        'target_id' => 4,
+      ],
+      [
+        'subject' => 'Email about new seeds',
+        'activity_type_id' => 'Email',
+        'location' => 'Texas',
+        'details' => 'We just got new seeds.',
+        // Jane Smith.
+        'source_contact_id' => 3,
+        // John Doe.
+        'assignee_id' => 4,
+        // John Smith.
+        'target_id' => 2,
+      ],
+    ];
+
+    foreach ($activities as $activity) {
+      $civicrm_api->save('Activity', $activity);
     }
 
     drupal_flush_all_caches();
