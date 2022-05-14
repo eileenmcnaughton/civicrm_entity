6a7aef311553a1f9a91ff995a5b0161fa7dcf362
jitendrapurohit <jitendra@fuzion.co.nz>
2022-05-14 14:50:17 +0530

    Use fieldformatter to access custom contact reference field values

--- src/SupportedEntities.php
+++ src/SupportedEntities.php
@@ -122,7 +122,7 @@ final class SupportedEntities {
         'delete' => [],
       ],
     ];
-    $civicrm_entity_info['civicrm_contact'] = $civicrm_entity_info['civicrm_contact_reference'] = [
+    $civicrm_entity_info['civicrm_contact'] = [
       'civicrm entity label' => t('Contact'),
       'civicrm entity name' => 'contact',
       'label property' => 'display_name',
@@ -142,7 +142,6 @@ final class SupportedEntities {
         'display_name' => TRUE,
       ],
     ];
-    $civicrm_entity_info['civicrm_contact_reference']['permissions']['view'][] = 'access all custom data';
     $civicrm_entity_info['civicrm_contribution'] = [
       'civicrm entity label' => t('Contribution'),
       'civicrm entity name' => 'contribution',
