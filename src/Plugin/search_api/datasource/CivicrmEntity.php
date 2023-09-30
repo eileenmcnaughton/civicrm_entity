3f75a9a780d6bae0272349421997db37a072e7fe
puresyntax71 <34715246+puresyntax71@users.noreply.github.com>
2023-09-30 13:06:04 +0800

    Check if connection exists.

--- src/Plugin/search_api/datasource/CivicrmEntity.php
+++ src/Plugin/search_api/datasource/CivicrmEntity.php
@@ -15,7 +15,8 @@ class CivicrmEntity extends ContentEntity {
     $datasource = parent::create($container, $configuration, $plugin_id, $plugin_definition);
 
     $civicrm_connection_name = drupal_valid_test_ua() ? 'civicrm_test' : 'civicrm';
-    if (Database::getConnectionInfo($civicrm_connection_name)) {
+    $civicrm_database_info = Database::getConnectionInfo($civicrm_connection_name);
+    if (isset($civicrm_database_info['default'])) {
       $datasource->setDatabaseConnection(Database::getConnection('default', $civicrm_connection_name));
     }
 
