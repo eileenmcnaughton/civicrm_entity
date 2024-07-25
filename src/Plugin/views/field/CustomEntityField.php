c17c08fe999ea84d1d59b720cdeff81dd4ae5dbd
Mark Hanna <mark@skvare.com>
2024-07-25 11:34:32 -0600

    custom field Views plugin, test not using createEntity

--- src/Plugin/views/field/CustomEntityField.php
+++ src/Plugin/views/field/CustomEntityField.php
@@ -180,7 +180,7 @@ class CustomEntityField extends EntityField {
     ];
 
     if (($entity = $this->getEntity($values)) && isset($entity->{$this->definition['field_name']})) {
-      $entity = $this->createEntity($entity);
+      // $entity = $this->createEntity($entity);
 
       if (isset($this->aliases['id']) && isset($values->{$this->aliases['id']})) {
         $values->delta = $this->getDelta($values->{$this->aliases['id']});
