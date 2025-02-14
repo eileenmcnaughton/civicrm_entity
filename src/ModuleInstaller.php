80423cc395dde1c3a132196c2f3c1881c9cfa611
puresyntax71 <34715246+puresyntax71@users.noreply.github.com>
2025-02-15 01:32:00 +0800

    Update decorating service.

--- src/ModuleInstaller.php
+++ src/ModuleInstaller.php
@@ -13,23 +13,8 @@ use Drupal\Core\Update\UpdateHookRegistry;
 /**
  * Class ContentUninstallValidator.
  */
+#[AsDecorator(decorates: ExtensionModuleInstaller::class)]
 class ModuleInstaller extends ExtensionModuleInstaller {
-
-  /**
-   * The module installer.
-   *
-   * @var \Drupal\Core\Extension\ModuleInstallerInterface
-   */
-  protected $moduleInstaller;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct(ModuleInstallerInterface $module_installer, $root, ModuleHandlerInterface $module_handler, DrupalKernelInterface $kernel, Connection $connection, UpdateHookRegistry $update_registry, LoggerChannelFactoryInterface $logger_factory) {
-    $logger = $logger_factory->get('civicrm_entity');
-    parent::__construct($root, $module_handler, $kernel, $connection, $update_registry, $logger);
-    $this->moduleInstaller = $module_installer;
-  }
 
   /**
    * {@inheritdoc}
